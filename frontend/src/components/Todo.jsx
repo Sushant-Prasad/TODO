575f5c9a8fa28a54e83f0a329956cd5b44ebbded
Sushant-Prasad <sushantprasad77@gmail.com>
2025-06-17 22:34:38 +0530

    UPDATED

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -15,7 +15,7 @@ const Todo = () => {
         <form action="" className='flex gap-4 items-center'>
           <input
             type="text"
-            placeholder='Add a new task here'
+            placeholder='Add a new task'
             className='border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500'
           />
           <button><Plus size={25}/></button>
